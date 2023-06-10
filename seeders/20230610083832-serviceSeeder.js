07750793416fc82d8553bbc1eeb20dd26c29632b
marceop <marta.op18@gmail.com>
2023-06-10 16:18:41 +0200

    feat: adding services table

--- seeders/20230610083832-serviceSeeder.js
+++ seeders/20230610083832-serviceSeeder.js
@@ -3,15 +3,31 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert ('Services', [
+      
+      {id: 1,
+        name: "Teeth cleanings",
+        description: "Regular teeth cleanings are important to maintaining healthy teeth and gums.",
+        price: "50",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {id: 2,
+        name: "Tooth extractions",
+        description:"It is the removal of a tooth from its socket in the bone",
+        price: "80",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {id: 3,
+        name: " Root canal therapy",
+        description: "It is a dental procedure that is used to repair teeth that are infected or badly decayed",
+        price: "150",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
